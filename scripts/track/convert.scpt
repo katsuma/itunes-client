5b866c9445cbdbfd9ca911ed29edc08ad7c2506c
ryo katsuma <katsuma@gmail.com>
2013-06-30 02:32:38 +0900

    Fix Itunes::Track#convert error

--- scripts/track/convert.scpt
+++ scripts/track/convert.scpt
@@ -4,7 +4,8 @@ on run argv
     set specified_track to (some track whose persistent ID is persistent_id)
     set loc to (get location of specified_track)
 
-    set converted_track to (convert specified_track)
+    set converted_tracks to (convert specified_track)
+    set converted_track to item 1 of converted_tracks
     return persistent ID of converted_track
   end tell
 end run
--- spec/itunes/track_spec.rb
+++ spec/itunes/track_spec.rb
@@ -40,11 +40,10 @@ describe Track do
         and_return([Track.new(persistent_id: new_persistent_id)])
     end
 
-    it 'returns an array of new Track' do
-      converted_tracks = convert
-      expect(converted_tracks).to be_an(Array)
-      expect(converted_tracks.size).to eq(1)
-      expect(converted_tracks.first.persistent_id).to eq(new_persistent_id)
+    it 'returns a new Track' do
+      converted_track = convert
+      expect(converted_track).to be_a(Track)
+      expect(converted_track.persistent_id).to eq(new_persistent_id)
     end
   end
 
